341985fc2f7f6946e85665880a3aaa4ebb160f98
ge47hik <ge47hik@mytum.de>
2022-07-14 17:39:01 +0200

    Check

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,17 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "ben";
-        } else { // TODO extend the programm here
+        }
+        else if(query.contains("plus")){
+            String left = query.substring(17, 18);
+            String right = query.substring(24,query.length()-1);
+            left = left.strip();
+            right = right.strip();
+            int numberOne = Integer.parseInt(left);
+            int numberTwo = Integer.parseInt(right);
+            return left + right;
+        }
+        else {// TODO extend the programm here
             return "";
         }
     }
